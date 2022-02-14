c47f95baf86368e9799d8bffd1336ad08c7a2881
Amagaraï Guindo <amagarai6@gmail.com>
2022-02-14 14:40:54 +0000

    commit 3

--- src/main/java/com/example/apitutorat/users/repository/UsersRepository.java
+++ src/main/java/com/example/apitutorat/users/repository/UsersRepository.java
@@ -3,10 +3,15 @@ package com.example.apitutorat.users.repository;
 import com.example.apitutorat.users.Utulisateur;
 import com.example.apitutorat.users.model.Tuteur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UsersRepository extends JpaRepository<Utulisateur, Long> {
     public List<Tuteur> findByAddresseAndSpecialite(String ville, String specialite);
-    //public List<Utulisateur> findByEtat_Activer();
+
+    @Query("SELECT searc FROM Utulisateur searc WHERE searc.etat='ACTIVER'")
+    public List<Utulisateur> lister();
+
+    public List<Utulisateur> findByEtat_Activer();
 }
